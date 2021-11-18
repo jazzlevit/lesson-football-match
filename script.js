728322d6876784a8cb0fac0313f3fc32a2814826
Alex Stetsenko <jazzlevit@gmail.com>
2021-11-18 19:02:57 +0200

    script.js file is created

---
+++ script.js
@@ -0,0 +1,9 @@
+/**
+ * Please finish the lesson by adding needed js:
+ * - You can write code with jquery OR VanillaJs
+ * - but are better to try them both, two variants are recommended for learning
+*/
+
+// Jquery
+
+// VanillaJs
