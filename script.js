b175b60af3675ca22c577761f488a839f66c3215
Alex Stetsenko <jazzlevit@gmail.com>
2021-11-18 19:05:26 +0200

    Example of errors and result box

--- script.js
+++ script.js
@@ -2,6 +2,10 @@
  * Please finish the lesson by adding needed js:
  * - You can write code with jquery OR VanillaJs
  * - but are better to try them both, two variants are recommended for learning
+ *
+ *
+ * - To display errors please use .matchError class
+ * - To display match result please use .matchResult class
 */
 
 // Jquery
