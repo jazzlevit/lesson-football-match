3407c4d5c63b4bf627f0c790346d530c8f9b0b1e
Alex Stetsenko <jazzlevit@gmail.com>
2021-11-19 02:53:12 +0200

    VanillaJs solution

--- script.js
+++ script.js
@@ -11,3 +11,75 @@
 // Jquery
 
 // VanillaJs
+
+const formResultDelay = 2000;
+
+const matchResultBox = document.querySelector('.matchResult');
+const matchErrorBox = document.querySelector('.matchError');
+
+const startMatchButton = document.getElementById('startMatch');
+
+startMatchButton.addEventListener('click', function(event) {
+    startMatch();
+});
+
+const startMatch = function () {
+    clearMatch();
+
+    if (validateMatch() === false) {
+        return;
+    }
+
+    matchResultBox.classList.remove('hide');
+
+    setTimeout(function() {
+        const scoreFirst = randomIntFromInterval(0, 10);
+        const scoreSecond = randomIntFromInterval(0, 10);
+
+        setMatchResult(`${scoreFirst} : ${scoreSecond}`)
+    }, formResultDelay);
+}
+
+const validateMatch = function () {
+    const team1 = document.getElementById('team1');
+    const team2 = document.getElementById('team2');
+
+    if (team1.value === '' || team2.value === '') {
+        setMatchError('Please select teams');
+
+        return false;
+    }
+
+    if (team1.value === team2.value) {
+        setMatchError('Teams should be different');
+
+        return false;
+    }
+
+    return true;
+}
+
+const clearMatch = function () {
+    matchResultBox.innerHTML = 'Loading ...';
+    matchErrorBox.innerHTML = '';
+
+    matchResultBox.classList.add('hide');
+    matchErrorBox.classList.add('hide');
+}
+
+const randomIntFromInterval = function(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+const setMatchResult = function (text) {
+    matchResultBox.innerHTML = text;
+
+    matchResultBox.classList.remove('hide');
+}
+
+const setMatchError = function (text) {
+    matchResultBox.classList.add('hide');
+    matchErrorBox.classList.remove('hide');
+
+    matchErrorBox.innerHTML = text;
+}
